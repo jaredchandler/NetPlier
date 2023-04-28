df8a224b9ef41300d4f0f328e704d10082e28e07
jaredchandler <jaredchandler@users.noreply.github.com>
2023-04-28 11:28:51 -0400

    Update getkw.py

--- netplier/getkw.py
+++ netplier/getkw.py
@@ -23,3 +23,31 @@ def get_true_keyword(protocol_type, message):
         print("The TestName is not given known method for detecting direction.")
 
     return kw
+
+def get_true_keyword_updated(protocol_type, message):
+    if protocol_type == "dhcp":
+        kw = message.data[0:1]
+    elif protocol_type == "dnp3":
+        kw = message.data[3:4]
+    elif protocol_type == "ftp":
+        kw = re.split(" |-|\r|\n", message.data.decode())[0]
+    elif protocol_type ==  "icmp":
+        kw = message.data[0:1]
+    elif protocol_type == "modbus":
+        kw = message.data[7:8]
+    elif protocol_type == "ntp":
+        kw = message.data[0] & 0x07
+    elif protocol_type == "smb":
+        # Need to verify
+        kw = message.data[4+4]
+    elif protocol_type == "smb2":
+        # Need to verify
+        kw = struct.unpack("<H", message.data[4+12:4+12+2])[0]
+    elif protocol_type == "tftp":
+        kw = message.data[0:2]
+    elif protocol_type == "zeroaccess":
+        kw = message.data[4:8]
+    else:
+        print("The TestName is not given known method for detecting direction.")
+
+    return kw
